65a193bab3056340fba381f30156417ae3efc26d
magizdev <magizdev@gmail.com>
2013-01-18 07:45:50 +0800

    ddds

--- EasyTask/src/com/magizdev/easytask/viewmodel/TaskListAdapter.java
+++ EasyTask/src/com/magizdev/easytask/viewmodel/TaskListAdapter.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
