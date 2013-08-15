f8cea761bfd66354f11bbe673de7aa465dfd2e10
zhali17 <zhali17@zhali17-ubuntu.(none)>
2013-08-15 04:38:01 -0400

    update
    Signed-off-by:  <>

--- authentication/app.js
+++ authentication/app.js
@@ -30,19 +30,12 @@ app.configure('production', function(){
 });
 
 // Routes
-app.get('/ranklist1', function(req, res) {
-  Rank.listScore(1, function(error, ranks){
-    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    res.end(ranks, 'utf-8');
-  });
-});
-
 app.post('/rankadd', function(req, res) {
   var username=req.body.username;
-  console.log("here");
-  console.log(username);
   var score=req.body.score;
-  var mode=req.body.mode
+  var mode=req.body.mode;
+  var imei=req.body.imei;
+  var game=req.body.game;
   Rank.addScore(username, score, mode, function(err, user) {
     if(err) console.log(err);
     //res.writeHead(200, {'Content-Type':'text/html'});
@@ -50,35 +43,11 @@ app.post('/rankadd', function(req, res) {
   });
 });
 
-app.get('/ranklist2', function(req, res) {
-  Rank.listScore(2, function(error, ranks){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(ranks, 'utf-8');
-  });
-});
-app.get('/ranklist3', function(req, res) {
-  Rank.listScore(3, function(error, ranks){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(ranks, 'utf-8');
-  });
-});
-
 app.get('/ranklist', function(req, res) {
-  var level=req.query.level;
-  Rank.listScore(level, function(error, ranks){
+  var mode=req.query.mode;
+  Rank.listScore(mode, function(error, ranks){
     res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
     res.end(ranks, 'utf-8');
-  });
-});
-
-app.get('/rankadd', function(req, res) {
-  var username=req.query.username;
-  var score=req.query.score;
-  var mode=req.query.mode
-  Rank.addScore(username, score, mode, function(err, user) {
-    if(err) throw err;
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end('', 'utf-8');
   });
 });
 
