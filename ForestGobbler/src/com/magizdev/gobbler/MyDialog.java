d6f3aad2d701bb2468bc6783f5a974675805c22d
magizdev <magizdev@gmail.com>
2013-08-19 21:47:40 +0800

    update

--- ForestGobbler/src/com/magizdev/gobbler/MyDialog.java
+++ ForestGobbler/src/com/magizdev/gobbler/MyDialog.java
@@ -1,7 +1,6 @@
 package com.magizdev.gobbler;
 
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -16,25 +15,18 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -49,7 +41,6 @@ public class MyDialog extends Dialog implements OnClickListener {
 	private RatingBar ratingBar;
 	private int rating;
 	private String scoreString;
-	private SDCardUtil sdCardUtil;
 
 	public MyDialog(Context context, GameView gameview, String msg, Score score) {
 		super(context, R.style.dialog);
@@ -57,12 +48,9 @@ public class MyDialog extends Dialog implements OnClickListener {
 		this.context = context;
 		this.score = score;
 		this.setContentView(R.layout.dialog_view);
-		sdCardUtil = new SDCardUtil(context);
 		TextView text_msg = (TextView) findViewById(R.id.text_message);
 		TextView text_score = (TextView) findViewById(R.id.text_score);
-		ImageButton btn_next = (ImageButton) findViewById(R.id.next_imgbtn);
-		ImageButton btn_replay = (ImageButton) findViewById(R.id.replay_imgbtn);
-		ImageButton btn_upload = (ImageButton) findViewById(R.id.upload_imgbtn);
+		Button btn_replay = (Button) findViewById(R.id.replay_btn);
 		ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
 		int rating = 4;
 		if (gameview.getGameMode() == GameView.HARD_MODE) {
@@ -83,94 +71,38 @@ public class MyDialog extends Dialog implements OnClickListener {
 		scoreString = text_score.getText().toString()
 				.replace("$", String.valueOf(this.score.getScore()));
 		text_score.setText(scoreString);
-		btn_next.setOnClickListener(this);
 		btn_replay.setOnClickListener(this);
-		btn_upload.setOnClickListener(this);
 
 		this.setCancelable(false);
 	}
 
 	@Override
 	public void onClick(View v) {
-		int ratingPicId = 0;
-		switch (this.rating) {
-		case 0:
-		case 1:
-			ratingPicId = R.drawable.rating1;
-			break;
-		case 2:
-			ratingPicId = R.drawable.rating2;
-			break;
-		case 3:
-			ratingPicId = R.drawable.rating3;
-			break;
-		case 4:
-			ratingPicId = R.drawable.rating4;
-			break;
-		case 5:
-			ratingPicId = R.drawable.rating5;
-			break;
-		default:
-			ratingPicId = R.drawable.rating1;
-			break;
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this.context);
+		String userName = prefs.getString(SettingActivity.USERNAME_TAG,
+				"Player");
+		int intScore = score.getScore();
+		int mode = 1;
+		if (gameview.getMode() == GameView.HARD_MODE)
+			mode = 2;
+		if (gameview.getMode() == GameView.ENDLESS_MODE)
+			mode = 3;
+		String stringUrl = "http://gamerank.ap01.aws.af.cm/rankAdd";
+		TelephonyManager tmManager = (TelephonyManager) context
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		String imeiString = tmManager.getDeviceId();
+		ConnectivityManager connMgr = (ConnectivityManager) this.context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo != null && networkInfo.isConnected()) {
+			new UploadScoreTask().execute(stringUrl, userName,
+					Integer.toString(mode), Integer.toString(intScore),
+					imeiString);
 		}
-
-		switch (v.getId()) {
-		case R.id.upload_imgbtn:
-			SharedPreferences prefs = PreferenceManager
-					.getDefaultSharedPreferences(this.context);
-			String userName = prefs.getString(SettingActivity.USERNAME_TAG,
-					"Player");
-			int intScore = score.getScore();
-			int mode = 1;
-			if (gameview.getMode() == GameView.HARD_MODE)
-				mode = 2;
-			if (gameview.getMode() == GameView.ENDLESS_MODE)
-				mode = 3;
-			String stringUrl = "http://gamerank.ap01.aws.af.cm/rankAdd";
-			TelephonyManager tmManager = (TelephonyManager) context
-					.getSystemService(Context.TELEPHONY_SERVICE);
-			String imeiString = tmManager.getDeviceId();
-			ConnectivityManager connMgr = (ConnectivityManager) this.context
-					.getSystemService(Context.CONNECTIVITY_SERVICE);
-			NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-			if (networkInfo != null && networkInfo.isConnected()) {
-				new UploadScoreTask().execute(stringUrl, userName,
-						Integer.toString(mode), Integer.toString(intScore),
-						imeiString);
-			}
-		case R.id.replay_imgbtn:
-			this.dismiss();
-			score.Clear();
-			gameview.startPlay(gameview.getGameMode());
-			break;
-		case R.id.next_imgbtn:
-			Drawable ratingPic = context.getResources()
-					.getDrawable(ratingPicId);
-			Bitmap bitmap = Bitmap.createBitmap(269, 141,
-					Bitmap.Config.ARGB_8888);
-			Canvas canvas = new Canvas(bitmap);
-			ratingPic.setBounds(0, 0, 269, 141);
-			ratingPic.draw(canvas);
-			Paint paint = new Paint();
-			paint.setColor(0xFF60ADFF);
-			paint.setTextSize(20);
-			canvas.drawText(scoreString, 135, 50, paint);
-			boolean scoreFileSaved = sdCardUtil.saveBitmap(bitmap);
-
-			Intent shareScoreIntent = new Intent(Intent.ACTION_SEND);
-			String share = context.getResources().getString(R.string.share)
-					.replace("$", String.valueOf(score.getScore()));
-			shareScoreIntent.putExtra(Intent.EXTRA_TEXT, share);
-			shareScoreIntent.setType("text/plain");
-			if (scoreFileSaved) {
-				shareScoreIntent.putExtra(Intent.EXTRA_STREAM, Uri
-						.fromFile(new File(sdCardUtil.getImageFileFullName())));
-				shareScoreIntent.setType("image/png");
-			}
-			context.startActivity(shareScoreIntent);
-			break;
-		}
+		this.dismiss();
+		score.Clear();
+		gameview.startPlay(gameview.getGameMode());
 	}
 
 	private class UploadScoreTask extends AsyncTask<String, Void, String> {
