0db2991def45dcb095a5656b59fa23cc542b39b3
zhali17 <magizdev@gmail.com>
2013-10-21 16:25:34 +0800

    update

--- DayPlan/src/com/magizdev/dayplan/DayPlanActivity.java
+++ DayPlan/src/com/magizdev/dayplan/DayPlanActivity.java
@@ -1,12 +1,8 @@
 package com.magizdev.dayplan;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.magizdev.dayplan.viewmodel.DayTaskAdapter;
@@ -45,31 +41,31 @@ public class DayPlanActivity extends NavigationBaseActivity {
 		handler.removeMessages(0);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.activity_day_plan, menu);
-
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle presses on the action bar items
-		Intent intent = new Intent();
-		switch (item.getItemId()) {
-		case R.id.action_mark:
-			intent.setClass(this, BacklogItemActivity.class);
-			startActivity(intent);
-			return true;
-		case R.id.action_report:
-			intent.setClass(this, PieChartBuilder.class);
-			startActivity(intent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		MenuInflater inflater = getMenuInflater();
+//		inflater.inflate(R.menu.activity_day_plan, menu);
+//
+//		return super.onCreateOptionsMenu(menu);
+//	}
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle presses on the action bar items
+//		Intent intent = new Intent();
+//		switch (item.getItemId()) {
+//		case R.id.action_mark:
+//			intent.setClass(this, BacklogItemActivity.class);
+//			startActivity(intent);
+//			return true;
+//		case R.id.action_report:
+//			intent.setClass(this, PieChartBuilder.class);
+//			startActivity(intent);
+//			return true;
+//		default:
+//			return super.onOptionsItemSelected(item);
+//		}
+//	}
 
 	@Override
 	protected int getMyPostion() {
