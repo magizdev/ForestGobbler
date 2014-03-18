b5bb7d4809c32b849186db1c24c85f32434cfd93
zhali17 <zhali17@zhali17-w82.ca.com>
2014-03-18 18:53:10 +0800

    update

---
+++ DayPlan_VersionOne/src/com/magizdev/dayplan/versionone/util/BacklogComparator.java
@@ -0,0 +1,15 @@
+package com.magizdev.dayplan.versionone.util;
+
+import java.util.Comparator;
+
+import com.magizdev.dayplan.versionone.model.BacklogItem;
+
+public class BacklogComparator implements Comparator<BacklogItem> {
+
+	@Override
+	public int compare(BacklogItem lhs, BacklogItem rhs) {
+		
+		return 0;
+	}
+
+}
